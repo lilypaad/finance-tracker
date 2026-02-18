6a81844d893930b3e8e45a77ede8bd7c30568b72
Lily Tarpey <lilytarpey.dev@pm.me>
2026-02-19 02:44:00 +0800

    add header to signup page

--- src/app/(auth)/signup/[[...signup]]/page.tsx
+++ src/app/(auth)/signup/[[...signup]]/page.tsx
@@ -9,6 +9,7 @@ import {
 
 import SignupForm from "@/components/signup-form";
 import { redirect } from "next/navigation";
+import { HeaderLogo } from "@/components/header-logo";
 
 export default async function SignUpPage() {
   const session = await getSession();
@@ -18,6 +19,16 @@ export default async function SignUpPage() {
   }
 
   return (
+    <>
+    <header className="bg-linear-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex items-center justify-between mb-14">
+          <div className="flex items-center lg:gap-x-16">
+            <HeaderLogo />
+          </div>
+        </div>
+      </div>
+    </header>
     <main className="flex h-full flex-col items-center justify-center">
       <Card className='w-xl max-w-lg mx-auto mt-12'>
         <CardHeader>
@@ -28,5 +39,6 @@ export default async function SignUpPage() {
         </CardContent>
       </Card>
     </main>
+    </>
   )
 }
