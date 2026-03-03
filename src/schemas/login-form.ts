9031ecd0acf4d1b5e2b86d6de0d24bda24a5d228
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-03 23:46:52 +0800

    fix some indentation

--- src/schemas/login-form.ts
+++ src/schemas/login-form.ts
@@ -1,18 +1,18 @@
 import * as z from "zod";
 
 export const LoginFormSchema = z.object({
-    email: z.email({ error: "Please enter a valid email" }).trim(),
-    password: z
-      .string()
-      .min(1, { error: "Please enter your password" })
-      .trim(),
+  email: z.email({ error: "Please enter a valid email" }).trim(),
+  password: z
+    .string()
+    .min(1, { error: "Please enter your password" })
+    .trim(),
 });
 
-export type LoginFormState = 
+export type LoginFormState =
   | {
     errors?: {
-        email?: string[];
-        password?: string[];
+      email?: string[];
+      password?: string[];
     }
     message?: string
   }
--- src/schemas/signup-form.ts
+++ src/schemas/signup-form.ts
@@ -1,24 +1,24 @@
 import * as z from "zod";
 
 export const SignupFormSchema = z.object({
-    email: z.email({ error: "Invalid email" }).trim(),
-    password: z
-      .string()
-      .min(8, { error: "Password must be at least 8 characters" })
-      .max(64, { error: "Password must be at most 64 characters"})
-      .regex(/[a-zA-Z]/, { error: "Password must contain a letter" })
-      .regex(/[0-9]/, { error: "Password must contain a number" }),
-    firstName: z.string().max(64, { error: "Name is too long" }).trim(),
-    lastName: z.string().max(64, { error: "Name is too long" }).trim(),
+  email: z.email({ error: "Invalid email" }).trim(),
+  password: z
+    .string()
+    .min(8, { error: "Password must be at least 8 characters" })
+    .max(64, { error: "Password must be at most 64 characters" })
+    .regex(/[a-zA-Z]/, { error: "Password must contain a letter" })
+    .regex(/[0-9]/, { error: "Password must contain a number" }),
+  firstName: z.string().max(64, { error: "Name is too long" }).trim(),
+  lastName: z.string().max(64, { error: "Name is too long" }).trim(),
 });
 
-export type SignupFormState = 
+export type SignupFormState =
   | {
     errors?: {
-        email?: string[];
-        password?: string[];
-        firstName?: string[];
-        lastName?: string[];
+      email?: string[];
+      password?: string[];
+      firstName?: string[];
+      lastName?: string[];
     }
     message?: string
   }
