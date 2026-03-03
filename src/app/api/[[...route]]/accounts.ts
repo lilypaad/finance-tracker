f7cbc4b9d7a8b5b3dbd1e76ae2b7d227396a4595
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-03 20:52:32 +0800

    only return accounts from logged in user

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,20 +1,28 @@
 import { Hono } from "hono";
-
 import { drizzle } from "drizzle-orm/node-postgres";
+import { eq } from "drizzle-orm";
+
 import { accounts, users } from "@/db/schema";
 
 const db = drizzle({ connection: process.env.DRIZZLE_DATABASE_URL!, casing: "snake_case" });
 
 const app = new Hono()
   .get("/", async (c) => {
+    const auth = c.get("jwtPayload");
+    
+    if(!auth?.user?.id) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
     const data = await db.select({
       id: accounts.id,
       name: accounts.name,
       userId: accounts.userId,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.id, auth.user.id));
 
     return c.json({ data });
-  })
+  });
 
 export default app;
