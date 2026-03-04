08ed0f9ade33ef8e1ff3fbbdaa17619672b35b3f
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-04 21:02:08 +0800

    rename account bulk delete query file

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -15,7 +15,7 @@ import { Loader2, Plus } from "lucide-react";
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts";
 
 function AccountsPage() {
   const newAccount = useNewAccount();
