38818e69f2ba0a9f8f9e3121580c7c1d02f3dac7
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-05 18:28:38 +0800

    add transactions table to db with relations

---
+++ drizzle/0006_lucky_blob.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "transactions" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "transactions_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"amount" integer NOT NULL,
+	"payee" varchar,
+	"notes" varchar,
+	"date" timestamp NOT NULL,
+	"account_id" integer NOT NULL,
+	"category_id" integer,
+	"user_id" integer NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_account_id_accounts_id_fk" FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,5 @@
+import * as z from "zod";
+
 import { 
   pgTable, 
   pgEnum,
@@ -6,7 +8,7 @@ import {
   varchar,
   uniqueIndex, 
 } from "drizzle-orm/pg-core";
-
+import { relations } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 
 const timestamps = {
@@ -31,6 +33,10 @@ export const users = pgTable(
   (table) => [ uniqueIndex("email_idx").on(table.email) ]
 );
 
+export const usersRelations = relations(users, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
 export const accounts = pgTable(
   "accounts", 
   {
@@ -40,6 +46,10 @@ export const accounts = pgTable(
   },
 );
 
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
 export const categories = pgTable(
   "categories",
   {
@@ -47,7 +57,43 @@ export const categories = pgTable(
     name: varchar("name").notNull(),
     userId: integer("user_id").references(() => users.id, { onDelete: "cascade" }),
   }
-)
+);
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
+export const transactions = pgTable(
+  "transactions",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    amount: integer("amount").notNull(),
+    payee: varchar("payee"),
+    notes: varchar("notes"),
+    date: timestamp("date", { mode: "date" }).notNull(),
+    accountId: integer("account_id").references(() => accounts.id, { onDelete: "cascade" }).notNull(),
+    categoryId: integer("category_id").references(() => categories.id, { onDelete: "set null" }),
+    userId: integer("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+  }
+);
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  category: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+  user: one(users, {
+    fields: [transactions.userId],
+    references: [users.id],
+  }),
+}));
 
 export const insertAccountSchema = createInsertSchema(accounts);
 export const insertCategorySchema = createInsertSchema(categories);
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
