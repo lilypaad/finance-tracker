53a33943b4b59ff3613c5402abb213ab6c29e600
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-17 21:15:55 +0800

    transaction history page columns

---
+++ src/app/(dashboard)/transactions/columns.tsx
@@ -0,0 +1,157 @@
+"use client"
+
+import { InferResponseType } from "hono"
+import { client } from "@/lib/hono"
+import { format } from "date-fns"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+
+import { Actions } from "./actions"
+import { cn, convertAmountFromMiliunits, formatCurrency } from "@/lib/utils"
+
+export type Transaction = InferResponseType<typeof client.api.transactions.$get, 200>["data"][0]
+
+export const columns: ColumnDef<Transaction>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+
+  {
+    accessorKey: "date",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const date = row.getValue("date") as Date;
+
+      return (
+        <span>
+          {format(date, "dd MMMM, yyyy")}
+        </span>
+      );
+    }
+  },
+
+  {
+    accessorKey: "account",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Account
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      return (
+        <span>
+          {row.original.account.name}
+        </span>
+      );
+    }
+  },
+
+  {
+    accessorKey: "category",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Category
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      return (
+        <span>
+          {row.original.category ?? "-"}
+        </span>
+      );
+    }
+  },
+
+  {
+    accessorKey: "payee",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Payee
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+
+      return (
+        <span
+          className={cn(
+            "text-xs font-medium px-3.5 py-2.5",
+            amount < 0 ? "text-rose-600" : "text-emerald-700"
+          )}
+        >
+          {formatCurrency(convertAmountFromMiliunits(amount))}
+        </span>
+      );
+    }
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => <Actions id={String(row.original.id)} />,
+  },
+];
