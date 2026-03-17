5f4ba5680c7b9b525ec96ef43e9905a8c743ab13
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-17 18:37:09 +0800

    add transactions page

---
+++ src/app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { DataTable } from "@/components/data-table"
+import { columns } from "./columns"
+import { Loader2, Plus } from "lucide-react";
+
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
+import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+
+function TransactionsPage() {
+  const newTransaction = useNewTransaction();
+  const deleteTransactions = useBulkDeleteTransactions();
+  const transactionsQuery = useGetTransactions();
+  const transactions = transactionsQuery.data || [];
+
+  const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending;
+
+  if(isDisabled) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-125 w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm px-4 py-6">
+        <CardHeader className="gap-y-2 lg:flex lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+            Transaction History
+          </CardTitle>
+          <Button onClick={newTransaction.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={transactions}
+            filterKey="name"
+            onDelete={(rows) => {
+              const ids = rows.map((r) => r.original.id);
+              deleteTransactions.mutate({ ids });
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default TransactionsPage;
