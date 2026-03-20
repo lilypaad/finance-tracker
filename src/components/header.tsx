9fce6fd2673abb20f0494186351a4590a994e0eb
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-20 20:43:29 +0800

    change spacing in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ export async function Header() {
   const session = await getSession();
 
   return (
-    <header className="bg-linear-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-34">
+    <header className="bg-linear-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-32">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
