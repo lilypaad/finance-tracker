3cd1d5fc650997e27e0cb9ba1f1b904edcaac0e0
Lily Tarpey <lilytarpey.dev@pm.me>
2026-03-20 20:40:01 +0800

    add account filters to header element

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import { HeaderLogo } from "@/components/header-logo";
 import { Navigation } from "@/components/navigation";
 import LogoutButton from "@/components/logout-button";
+import { Filters } from "@/features/summary/components/filters";
 import { getSession } from "@/lib/auth";
 
 export async function Header() {
   const session = await getSession();
 
   return (
-    <header className="bg-linear-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+    <header className="bg-linear-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-34">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
@@ -16,11 +17,14 @@ export async function Header() {
           </div>
           {session && <LogoutButton />}
         </div>
-      {session && (
-          <h1 className="text-2xl lg:text-4xl text-white font-medium">
-            Welcome back, {session.user.firstName}
-          </h1>
-      )}
+        {session && (
+          <div className="space-y-8">
+            <h1 className="text-2xl lg:text-4xl text-white font-medium">
+              Welcome back, {session.user.firstName}
+            </h1>
+            <Filters />
+          </div>
+        )}
       </div>
     </header>
   );
